92d70d488257f3a6c5ce912f4e8e30d591ab3c5b
Nami Sunami <nsunami@pm.me>
2025-11-16 18:43:11 +0100

    feat: overwrite the metadata file based on the data dictionary

--- scripts/generate_variables_measured.R
+++ scripts/generate_variables_measured.R
@@ -1,11 +1,14 @@
 source("scripts/load_data.R")
 
+# Define the path to the metadata file
+DATASET_DESCRIPTION_PATH <- here::here("dataset_description.json")
+
+# Load the RDS data 
 recalling_surrogates_data <- load_data()
 
+# Extract the data dictionary
 data_dictionary <- labelled::generate_dictionary(recalling_surrogates_data)
-
 codebook_table <- codebook::codebook_table(recalling_surrogates_data)
-
 variables_measured_tbl <- codebook_table |>
   dplyr::select(
     name,
@@ -15,8 +18,21 @@ variables_measured_tbl <- codebook_table |>
     # TODO: Extract the value labels and use them in the variableMeasured
   ) |>
   dplyr::mutate("@type" = "PropertyValue", .before = dplyr::everything())
-
+## Convert the existing data dictionary to JSON
 variables_measured_json <- jsonlite::toJSON(variables_measured_tbl, pretty = TRUE)
 
-# Write to the clipboard
-clipr::write_clip(variables_measured_json)
+# Add the data dictionary data to the metadata
+## Read the existing metadata
+existing_metadata <- jsonlite::fromJSON(DATASET_DESCRIPTION_PATH)
+## Remove the old element for variables measured
+existing_metadata[["variableMeasured"]] <- NULL
+## Add the updated element for the variables measured
+updated_metadata <- existing_metadata
+updated_metadata[["variableMeasured"]] <- jsonlite::fromJSON(variables_measured_json)
+## Save the `dataset_description.json`
+jsonlite::write_json(
+  updated_metadata,
+  DATASET_DESCRIPTION_PATH,
+  pretty = TRUE,
+  auto_unbox = TRUE
+)
